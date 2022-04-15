6b27967ebf68d0a7551cb41c19ee6d58f555dc71
Muchendu <muchendumaina66@gmail.com>
2022-04-15 14:46:52 +0300

    Functions&pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,7 @@
-#ifndef FUNC_POINTERS
-#define FUNC_POINTERS
-#include <stdlib.h>
-
+#ifndef STEVED_H
+#define STEVED_H
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-
 #endif
